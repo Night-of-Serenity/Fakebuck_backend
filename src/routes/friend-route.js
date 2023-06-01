8dd3dff28f987df4e060f9d031c4470c9379c373
Nightz <peerapat.ud@gmail.com>
2023-06-01 16:55:54 +0700

    edit cancel request route

--- src/routes/friend-route.js
+++ src/routes/friend-route.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 router.post("/:receiverId", friendController.addFriend);
 router.patch("/:requesterId", friendController.confirmedFriend);
-router.delete("/:receiverId/cancer", friendController.cancelRequest);
+router.delete("/:receiverId/cancel", friendController.cancelRequest);
 router.delete("/:friendId/unfriend", friendController.unfriend);
 router.delete("/:requesterId/reject", friendController.rejectRequest);
 module.exports = router;
